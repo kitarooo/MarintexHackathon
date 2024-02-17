ccaf409000e4ba93e932d9fe7710331a6fcad542
azamatshauenbekov <bravlstarstop05@mail.ru>
2024-02-17 13:56:27 +0600

    Initial commit

---
+++ src/main/java/backend/course/spring/marintexhackathon/MarintexHackathonApplication.java
@@ -0,0 +1,13 @@
+package backend.course.spring.marintexhackathon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarintexHackathonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarintexHackathonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/backend/course/spring/marintexhackathon/MarintexHackathonApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.course.spring.marintexhackathon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarintexHackathonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
