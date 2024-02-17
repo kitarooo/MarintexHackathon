50195c042ef39cb03b68138d546583b84aabcaba
kitarooo <bravlstarstop05@mail.ru>
2024-02-17 15:34:31 +0600

    test commit

---
+++ src/main/java/backend/course/spring/marintexhackathon/entity/Post.java
@@ -0,0 +1,17 @@
+package backend.course.spring.marintexhackathon.entity;
+
+import backend.course.spring.marintexhackathon.entity.base_entity.BaseEntity;
+import jakarta.persistence.Entity;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Getter
+@Setter
+@Builder
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class Post extends BaseEntity {
+
+}
---
+++ src/main/java/backend/course/spring/marintexhackathon/entity/Ship.java
@@ -0,0 +1,16 @@
+package backend.course.spring.marintexhackathon.entity;
+
+import backend.course.spring.marintexhackathon.entity.base_entity.BaseEntity;
+import jakarta.persistence.Entity;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Getter
+@Setter
+@Builder
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class Ship extends BaseEntity {
+}
