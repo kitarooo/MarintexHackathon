3583c75dc7226d65babc68edce635179da7dde8a
godofthesenewworld <godofthesenewworld@gmail.com>
2024-02-17 15:41:54 +0600

    updateEntities

--- src/main/java/backend/course/spring/marintexhackathon/entity/Comment.java
+++ src/main/java/backend/course/spring/marintexhackathon/entity/Comment.java
@@ -1,4 +1,10 @@
 package backend.course.spring.marintexhackathon.entity;
 
 public class Comment {
+    Integer AuthorID;
+    Integer PostID;
+    Integer LikeCNT;
+    Integer DisLikeCNT;
+    String Content;
+    String AuthorUserName;
 }
